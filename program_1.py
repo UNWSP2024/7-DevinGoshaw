ee6a00ad55e2b9cdf1b7b28b9bbba25770d0995a
DevinGoshaw <DRGoshaw@students.unwsp.edu>
2025-10-16 14:16:32 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -5,25 +5,28 @@
 #programmer:Devin Goshaw 
 #date:10/16/25
 #program: Rainfall program 
+def main():
+    months=[
+       "january", "Febuary", "March", "April", "May", "June", "July", "Agust", "September", "October", "November", "December"
+    ]
+    rainfall=[]
+    for month in months:
+        amount= float(input('Enter the total rainfall for ' + month +':'))
+        rainfall.append(amount)
 
-months=[
-    "january", "Febuary", "March", "April", "May", "June", "July", "Agust", "September", "October", "November", "December"
-]
-rainfall=[]
-for month in months:
-    amount= float(input('Enter the total rainfall for ' + month +':'))
-    rainfall.append(amount)
+    total_Rain=sum(rainfall)
+    average_Rain=total_Rain/12
 
-total_Rain=sum(rainfall)
-average_Rain=total_Rain/12
+    max_Rain=max(rainfall)
+    min_Rain=min(rainfall)
+    max_Month=months[rainfall.index(max_Rain)]
+    min_Month=months[rainfall.index(min_Rain)]
 
-max_Rain=max(rainfall)
-min_Rain=min(rainfall)
-max_Month=months[rainfall.index(max_Rain)]
-min_Month=months[rainfall.index(min_Rain)]
+    print("\n Rainfall Statistics ")
+    print("Total rainfall for the year: ", round(total_Rain, 2), "inches")
+    print("Average monthly rainfall: ", round(average_Rain, 2), "inches")
+    print("largest amount rainfall: ", max_Month, "=", round(max_Rain, 2), "inches ")
+    print("minimum amount of rainfall: ", min_Month, "=", round(min_Rain, 2), "inches ")
 
-print("\n Rainfall Statistics ")
-print("Total rainfall for the year: ", round(total_Rain, 2), "inches")
-print("Average monthly rainfall: ", round(average_Rain, 2), "inches")
-print("largest amount rainfall: ", max_Month, "=", round(max_Rain, 2), "inches ")
-print("minimum amount of rainfall: ", min_Month, "=", round(min_Rain, 2), "inches ")
+if __name__ == '__main__':
+    main()
