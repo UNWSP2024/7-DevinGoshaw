3e4ab007d9ea4b6ed70d54ccea9033ab6446f8cb
DevinGoshaw <DRGoshaw@students.unwsp.edu>
2025-10-16 14:05:08 -0500

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -8,3 +8,43 @@
 # Also include exception handling to deal with faulty input.
 # The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
 #    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+
+#programmer:Devin Goshaw 
+#Date: 10/16025
+#program: Coordinates program 
+
+import math
+
+def distance(point1, point2):
+    x1, y1, z1 = point1
+    x2, y2, z2 = point2
+
+    
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+    return dist
+
+
+def main():
+    print("This program calculates the distance between two 3D points.")
+
+    try:
+        x1 = float(input("Enter x1: "))
+        y1 = float(input("Enter y1: "))
+        z1 = float(input("Enter z1: "))
+        point1 = (x1, y1, z1)
+
+        x2 = float(input("Enter x2: "))
+        y2 = float(input("Enter y2: "))
+        z2 = float(input("Enter z2: "))
+        point2 = (x2, y2, z2)
+
+        result = distance(point1, point2)
+
+        print("The distance between", point1, "and", point2, "is:", round(result, 3))
+
+    except ValueError:
+        print("Error: Please enter valid numeric values for coordinates.")
+
+
+if __name__ == "__main__":
+    main()
